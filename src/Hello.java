d3c540e3d784a591b0c856f2171d3fc6369cc5f5
Rochak Sharma <sharmarochak348@gmail.com>
2021-07-06 23:26:51 +0530

    init commit

---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello, Rochak");
+        int myFistNumber = 4000;
+        int myLastOne = myFistNumber-1000;
+        System.out.println(myLastOne);
+    }
+}
